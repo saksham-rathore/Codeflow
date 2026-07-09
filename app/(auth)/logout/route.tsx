324ae5de0eac61cf0a8169cab7919a0d97f9c7a1
saksham762007 <samrathore07062007@gmail.com>
2026-07-09 22:36:04 +0530

    Remove obsolete files including configuration, styles, and components for authentication, as well as project setup files. This cleanup prepares the project for a more streamlined architecture.

--- app/(auth)/logout/route.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/app/lib/auth";
-import { headers } from "next/headers";
-
-export async function signOut() {
-  await auth.api.signOut({
-    headers: await headers(),
-  });
-}
