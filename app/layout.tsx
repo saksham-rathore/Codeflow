e83245d525e95748efc32575d006624356cad35a
saksham762007 <samrathore07062007@gmail.com>
2026-07-09 23:29:01 +0530

    Refactor authentication components and update project structure. Removed outdated files and improved organization for better maintainability.

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
