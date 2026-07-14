abeb989db39737b39075f0493ebb435d65cafd75
saksham762007 <samrathore07062007@gmail.com>
2026-07-14 11:01:06 +0530

    feat: initialize messages database schema and CRUD operations

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,13 +1,17 @@
+// import { role } from "better-auth/client";
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
   messages: defineTable({
-    author: v.id("users"),
-    body: v.string(),
+    conversationId: v.string(),
+    userId: v.string(),
+    role: v.union(
+      v.literal("user"),
+      v.literal("assistant"),
+      v.literal("system")
+    ),
+    content: v.string(),
+    createdAt: v.number(),
   }),
-  users: defineTable({
-    name: v.string(),
-    userName: v.string(),
-  }),
-}); 
+})
