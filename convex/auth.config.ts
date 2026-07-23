3738e8d662a3f3c22963f3f6998f09c97baca4d6
saksham762007 <samrathore07062007@gmail.com>
2026-07-23 12:02:27 +0530

    feat: initialize Convex API types and configure Clerk authentication provider

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from "convex/server";
 export default {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN!,
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN! ,
       applicationID: "convex",
     },
   ],
