5d4bfa5151a756c9a73ea17ac8a9074bb074d21a
saksham762007 <samrathore07062007@gmail.com>
2026-07-28 01:05:51 +0530

    feat: implement project hooks with optimistic updates and add file management mutation logices

--- convex/files.ts
+++ convex/files.ts
@@ -1,3 +1,96 @@
 import { v } from "convex/values";
 import { query, mutation } from "./_generated/server";
 import { verifyAuth } from "./auth";
+
+export const getFiles = query({
+    args: {
+        
+    },
+    handler: async (ctx, args) => {
+
+    }
+});
+
+
+export const getFile = query({
+    args: {
+
+    },
+    handler: async (ctx, args) => {
+
+    }
+});
+
+
+export const RenameFile = query({
+    args: {
+
+    },
+    handler: async (ctx, args) => {
+
+    }
+});
+
+
+export const RenameFolder = query({
+    args: {
+
+    },
+    handler: async (ctx, args) => {
+
+    }
+});
+
+
+export const create = mutation({
+    args: {
+        name: v.string(),
+        projectId: v.id("projects"),
+        parentId: v.optional(v.id("files")),
+        content: v.string()
+    },
+    handler: async (ctx, args) => {
+        const identity = await verifyAuth(ctx);
+
+        const project = await ctx.db.get("projects", args.projectId);
+
+        if (!project) {
+            throw new Error("Project not found")
+        };
+
+        if (project.ownerId !== identity.subject) {
+            throw new Error("Unauthorized to access this project");
+        }
+
+        // check if file with same name already exists in this parent folder
+        const files = await ctx.db
+            .query("files")
+            .withIndex("by_project_parent", (q) =>
+                q
+                    .eq("projectId", args.projectId)
+                    .eq("parentId", args.parentId)
+            )
+            .collect();
+
+        const existing = files.find(
+            (files) => files.name === args.name && files.type === "file"
+        );
+
+        if (existing) throw new Error("File already exists");
+
+        const now = Date.now();
+
+        await ctx.db.insert("files", {
+            projectId: args.projectId,
+            name: args.name,
+            parentId: args.parentId,
+            content: args.content,
+            type: "file",
+            updatedAt: now,
+        })
+
+        await ctx.db.patch("projects", args.projectId, {
+            updatedAt: now,
+        });
+    },
+});
