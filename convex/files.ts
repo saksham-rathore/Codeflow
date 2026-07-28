5ccb559a957870078c9f85ec27010edf784d1889
saksham762007 <samrathore07062007@gmail.com>
2026-07-28 13:57:20 +0530

    feat: implement file management queries and mutation for file creation

--- convex/files.ts
+++ convex/files.ts
@@ -4,10 +4,25 @@ import { verifyAuth } from "./auth";
 
 export const getFiles = query({
     args: {
-        
+        projectId: v.id("projects")
     },
     handler: async (ctx, args) => {
+        const identity = await verifyAuth(ctx);
 
+        const project = await ctx.db.get("projects", args.projectId);
+
+        if (!project) {
+            throw new Error("Project not found")
+        };
+
+        if (project.ownerId !== identity.subject) {
+            throw new Error("Unauthorized to access this project");
+        };
+
+        return await ctx.db
+            .query("files")
+            .withIndex("by_project", (q) => q.eq("projectId", args.projectId))
+            .collect();
     }
 });
 
